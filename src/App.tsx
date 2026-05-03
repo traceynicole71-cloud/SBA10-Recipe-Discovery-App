d0612c4bbbd570f20487dfe64c39aa1a2027a344
Tracey Roberts <traceynicole71@yahoo.com>
2026-05-03 14:31:36 -0400

    index ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FavoritesProvider } from './context/FavoritesContext';
 import { Navbar } from './components/Navbar';
---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export interface Category {
+    idCategory: string;
+    strCategory: string;
+    strCategoryThumb: string;
+    strCategoryDecription: string;
+}
+
+export interface Meal {
+    idMeal: string;
+    strMeal: string;
+    strMealThumb: string;
+    strInstructions?: string;
+    strCategory?: string;
+    strArea?: string;
+}
+
+export interface ApiResponse<T> {
+    meals: T[] | null;
+    categories: T[] | null;
+}
